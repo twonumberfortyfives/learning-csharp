79544d8298bcbe2686c7a9ea54afbc6bb8ba5e30
twonumberfortyfives <sma4no.7@gmail.com>
2024-11-28 23:19:27 +0200

    initialized

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+internal class Program
+{
+    private static string word = "world";
+    private static string Word(string word)
+    {
+        return word;
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine($"Hello {Word(word)}");
+    }
+}
